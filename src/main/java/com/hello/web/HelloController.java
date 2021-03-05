f0a35a6d0c0fc2317569c01712638a9bf94f9011
Celina Junker <junkerc2@nku.edu>
2021-03-05 13:55:30 -0500

    init commit

---
+++ src/main/java/com/hello/web/HelloController.java
@@ -0,0 +1,26 @@
+package com.hello.web;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World!!!";
+	}
+	
+	//using a request parameter... ?name=Bob
+	@GetMapping("")
+	public String sayHello(@RequestParam String name) {
+		return "Hello "+ name + "!!!";
+	}
+	
+	
+	
+	
+	
+}
